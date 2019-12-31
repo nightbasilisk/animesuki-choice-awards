611816b4ea69d4ea29cad6141c5ac2f905fad708
nightbasilisk <nightbasilisk@gmail.com>
2019-12-31 17:54:29 +0200

    prev sessions support

--- src/ChoiceAwards.js
+++ src/ChoiceAwards.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Button from 'antd/lib/button';
+import Modal from 'antd/lib/modal';
+import Table from 'antd/lib/table';
 import { Eligible } from './Eligible';
 import { Choices } from './Choices';
 import { PostPreview } from './PostPreview'
@@ -15,8 +18,9 @@ function emptyCatState(contest) {
 }
 
 export function ChoiceAwards(props) {
-    const { contest } = props;
+    const { contest, sessionId, sessions } = props;
     const seriesCache = store.namespace(`${contest.year}`);
+    const picksCache = store.namespace(sessionId);
 
     let [ post, update ] = useState({ 
         picks: emptyCatState(contest),
@@ -34,13 +38,52 @@ export function ChoiceAwards(props) {
             update(post);
             refresh(refreshId + 1);
             seriesCache('selected.list', post.selected.list);
+            picksCache('post.picks', post.picks);
         }
     };
 
     // console.log('post', post);
 
+    let [ showSessionsModal, updateSessionsModal ] = useState(false);
+
+    function loadPicks(sessionId) {
+        let otherCache = store.namespace(sessionId);
+        let picks = otherCache('post.picks');
+        post.picks = picks;
+        state.updatePost(post);
+    }
+
+    const columns =[{
+        key: 'time',
+        title: 'Session Time',
+        dataIndex: 'time',
+        render: t => new Date(t).toString().replace(/\(.*\)/, '')
+    }, {
+        key: 'actions',
+        dataIndex: 'sessionId',
+        render: sessionId => {
+            return (
+                <Button size="small" onClick={() => loadPicks(sessionId)}>Load Picks</Button>
+            );
+        }
+    }];
+
+    let sessionsModal = (
+        <Modal
+          title="Previous Sessions"
+          visible={showSessionsModal}
+          onOk={() => updateSessionsModal(false)}
+          onCancel={() => updateSessionsModal(false)}
+        >
+            <p>A new session is created every time you open this page. They are saved localy (not on a server), so they're not there if you changed browsers, computers or user profiles. Some sessions may be empty. Only sessions for the current contest phase are displayed.</p>
+            <Table rowKey={row => row.sessionId} columns={columns} dataSource={sessions}/>
+        </Modal>
+    );
+
     return (
         <div className="awards">
+
+            {sessionsModal}
 
             { contest.phase == 'main' ? 
                 <div className="eligible">
@@ -50,6 +93,9 @@ export function ChoiceAwards(props) {
                 : null }
 
             <div className="choices">
+                <div style={{ float: 'right' }}>
+                    <Button onClick={() => updateSessionsModal(true)} disabled={sessions.length == 0}>Load Previous Session</Button>
+                </div>
                 <h1>Choice Awards</h1>
                 <p><a href="https://www.youtube.com/watch?v=3Y3jE3B8HsE" target="_blank">Instant-Runoff Voting rules</a> apply. Your first choice applies so long as it's not the least popular, in which case your next choice is applied, et cetera. Repeats until 50%+1 concensus is achieved or nobody has any backup options left.</p>
                 <Choices contest={contest} state={state}/>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { ChoiceAwards } from './ChoiceAwards';
+import filter from 'lodash/filter';
+const uuid = require('uuid/v4');
 const el = el => document.querySelector(el);
+const store = require('store2');
 
 (async () => {
 
@@ -9,9 +12,47 @@ const el = el => document.querySelector(el);
     const contest = await contestRes.json();
 
     // console.log('contest', contest);
+
+    let sessionId = uuid();
+    const sessionsCache = store.namespace(`sessions-${contest.year}-${contest.phase}`);
+
+    let sessions = sessionsCache('list') || [];
+    
+    function picksLength(picks) {
+        if (picks == null) {
+            return 0;
+        }
+        
+        let count = 0;
+        for (let cat of Object.keys(picks)) {
+            count += picks[cat].length;
+        }
+
+        return count;
+    }
+
+    let relevantSessions = [];
+    for (let session of sessions) {
+        const picksCache = store.namespace(session.sessionId);
+        let picks = picksCache('post.picks');
+        if (picksLength(picks) > 0) {
+            relevantSessions.push(session)
+        }
+    }
+
+    relevantSessions.push({
+        time: Date.now(),
+        sessionId
+    });
+
+    relevantSessions.sort((a, b) => b.time - a.time);
+
+    sessionsCache('list', relevantSessions);
+
+    sessions = filter(relevantSessions, entry => entry.sessionId != sessionId);
     
     ReactDOM.render (
-        <ChoiceAwards contest={contest}/>, 
+        <ChoiceAwards contest={contest} sessionId={sessionId} sessions={sessions}/>, 
         el('#root')
     );
 })();
